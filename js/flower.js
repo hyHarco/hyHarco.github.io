a684d73615a5450299f7e6cc2e6bd16c5c2ca472
krex <ralwnsgus4715@gmail.com>
2023-04-03 12:19:41 +0900

    update effect

--- js/flower.js
+++ js/flower.js
@@ -53,10 +53,10 @@
         var defaults = {
             blowAnimations: ['blow-soft-left', 'blow-medium-left', 'blow-hard-left', 'blow-soft-right', 'blow-medium-right', 'blow-hard-right'],
             className: 'sakura',
-            fallSpeed: 5,
-            maxSize: 15,
+            fallSpeed: 6,
+            maxSize: 13,
             minSize: 10,
-            newOn: 900,
+            newOn: 1000,
             swayAnimations: ['sway-0', 'sway-1', 'sway-2', 'sway-3', 'sway-4', 'sway-5', 'sway-6', 'sway-7', 'sway-8']
         };
 
